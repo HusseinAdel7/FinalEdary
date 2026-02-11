9d95be1bc00f472e9e039e2f100aa77f4131c20e
AlaaAyesh <141708416+AlaaAyesh@users.noreply.github.com>
2026-02-11 22:36:42 +0200

    add items to side nav bar

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,34 +1,81 @@
-import { authGuard, permissionGuard } from '@abp/ng.core';
 import { Routes } from '@angular/router';
+import { authGuard, permissionGuard } from '@abp/ng.core';
 
 export const APP_ROUTES: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    loadComponent: () => import('./home/home.component').then(c => c.HomeComponent),
+    loadComponent: () =>
+      import('./home/home.component').then(c => c.HomeComponent),
   },
+
   {
     path: 'main-accounts',
-    loadChildren: () => import('./modules/main-accounts/main-accounts-module').then(m => m.MainAccountsModule),
+    // canActivate: [authGuard],
+    loadChildren: () =>
+      import('./modules/main-accounts/main-accounts-module')
+        .then(m => m.MainAccountsModule),
   },
+
+  {
+    path: 'sub-accounts',
+    // canActivate: [authGuard],
+    loadChildren: () =>
+      import('./modules/sub-accounts/sub-accounts-module')
+        .then(m => m.SubAccountsModule),
+  },
+
+  {
+    path: 'journal-entries',
+    // canActivate: [authGuard],
+    loadChildren: () =>
+      import('./modules/journal-entries/journal-entries-module')
+        .then(m => m.JournalEntriesModule),
+  },
+
+  {
+    path: 'items',
+    // canActivate: [authGuard],
+    loadChildren: () =>
+      import('./modules/items/items-module')
+        .then(m => m.ItemsModule),
+  },
+
+  {
+    path: 'warehouses',
+    // canActivate: [authGuard],
+    loadChildren: () =>
+      import('./modules/warehouses/warehouses-module')
+        .then(m => m.WarehousesModule),
+  },
+
   {
     path: 'suppliers',
-    loadChildren: () => import('./modules/suppliers/suppliers-module').then(m => m.SuppliersModule),
+    // canActivate: [authGuard],
+    loadChildren: () =>
+      import('./modules/suppliers/suppliers-module')
+        .then(m => m.SuppliersModule),
   },
+
+  // ABP Built-in Modules
   {
     path: 'account',
-    loadChildren: () => import('@abp/ng.account').then(c => c.createRoutes()),
+    loadChildren: () =>
+      import('@abp/ng.account').then(m => m.createRoutes()),
   },
   {
     path: 'identity',
-    loadChildren: () => import('@abp/ng.identity').then(c => c.createRoutes()),
+    loadChildren: () =>
+      import('@abp/ng.identity').then(m => m.createRoutes()),
   },
   {
     path: 'tenant-management',
-    loadChildren: () => import('@abp/ng.tenant-management').then(c => c.createRoutes()),
+    loadChildren: () =>
+      import('@abp/ng.tenant-management').then(m => m.createRoutes()),
   },
   {
     path: 'setting-management',
-    loadChildren: () => import('@abp/ng.setting-management').then(c => c.createRoutes()),
+    loadChildren: () =>
+      import('@abp/ng.setting-management').then(m => m.createRoutes()),
   },
 ];
--- angular/src/app/route.provider.ts
+++ angular/src/app/route.provider.ts
@@ -9,28 +9,89 @@ export const APP_ROUTE_PROVIDER = [
 
 function configureRoutes() {
   const routes = inject(RoutesService);
+
   routes.add([
-      {
-        path: '/',
-        name: '::Menu:Home',
-        iconClass: 'fas fa-home',
-        order: 1,
-        layout: eLayoutType.application,
-      },
-       {
-        path: 'main-accounts',
-        name: 'Main Accounts',
-        iconClass: 'fa-solid fa-list',
-        order: 2,
-        layout: eLayoutType.application,
-      },
-      ,
-       {
-        path: 'suppliers',
-        name: 'suppliers',
-        iconClass: 'fa-solid fa-list',
-        order: 3,
-        layout: eLayoutType.application,
-      },
+    {
+      path: '/',
+      name: '::Menu:Home',
+      iconClass: 'fas fa-home',
+      order: 1,
+      layout: eLayoutType.application,
+    },
+
+
+    {
+      path: '/accounting',
+      name: 'Accounting',
+      iconClass: 'fas fa-calculator',
+      order: 2,
+      layout: eLayoutType.application,
+    },
+    {
+      path: 'main-accounts',
+      name: 'Main Accounts',
+      iconClass: 'fa-solid fa-sitemap',
+      parentName: 'Accounting',
+      order: 1,
+      layout: eLayoutType.application,
+    },
+    {
+      path: 'sub-accounts',
+      name: 'Sub Accounts',
+      iconClass: 'fa-solid fa-diagram-project',
+      parentName: 'Accounting',
+      order: 2,
+      layout: eLayoutType.application,
+    },
+    {
+      path: 'journal-entries',
+      name: 'Journal Entries',
+      iconClass: 'fa-solid fa-book-open',
+      parentName: 'Accounting',
+      order: 3,
+      layout: eLayoutType.application,
+    },
+
+
+    {
+      path: '/purchases',
+      name: 'Purchases',
+      iconClass: 'fas fa-shopping-cart',
+      order: 3,
+      layout: eLayoutType.application,
+    },
+    {
+      path: 'suppliers',
+      name: 'Suppliers',
+      iconClass: 'fa-solid fa-truck',
+      parentName: 'Purchases',
+      order: 1,
+      layout: eLayoutType.application,
+    },
+
+
+    {
+      path: '/inventory',
+      name: 'Inventory',
+      iconClass: 'fas fa-boxes',
+      order: 4,
+      layout: eLayoutType.application,
+    },
+    {
+      path: 'items',
+      name: 'Items',
+      iconClass: 'fa-solid fa-box-open',
+      parentName: 'Inventory',
+      order: 1,
+      layout: eLayoutType.application,
+    },
+    {
+      path: 'warehouses',
+      name: 'Warehouses',
+      iconClass: 'fa-solid fa-warehouse',
+      parentName: 'Inventory',
+      order: 2,
+      layout: eLayoutType.application,
+    },
   ]);
 }
