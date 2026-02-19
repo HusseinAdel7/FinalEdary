2cca921620e08d6d8f0dfb2363456dfe468e155e
helhusseiny <helhusseiny@HELHUSSEINY-LAB>
2026-02-19 23:24:00 +0200

    Update Program and appmodule for hosting

--- src/Edary.HttpApi.Host/Program.cs
+++ src/Edary.HttpApi.Host/Program.cs
@@ -24,8 +24,8 @@ public class Program
             var builder = WebApplication.CreateBuilder(args);
 
              //🔥 مهم جدًا للـ Railway
-            //var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-            //builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
             builder.Host
                 .AddAppSettingsSecretsJson()
